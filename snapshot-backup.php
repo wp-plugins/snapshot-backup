21ace9eb823c70fa7e159572a3d6938c202ba2e6
versluis <versluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-18 17:39:43 +0000

    tagging version 1.6
    git-svn-id: https://plugins.svn.wordpress.org/snapshot-backup/tags/1.6@386334 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- snapshot-backup.php
+++ snapshot-backup.php
@@ -2,15 +2,15 @@
 /*
 Plugin Name: Snapshot Backup
 Plugin URI: http://wpguru.co.uk/2011/02/snapshot-backup/
-Description: Backs up your ENTIRE Wordpres site and sends it to an FTP archive. Excellent!
+Description: Backs up your ENTIRE Wordpress site and sends it to an FTP archive. Excellent!
 Author: Jay Versluis
-Version: 1.0
+Version: 1.6
 Author URI: http://wpguru.co.uk
 License: GPLv2 or later
 
 Copyright 2011 by Jay Versluis (email : versluis2000@yahoo.com)
 
-This is Version 1.0.5 as of 21/02/2011
+This is Version 1.6.2 as of 18/05/2011
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -52,6 +52,8 @@ function snapshot() {
     $opt_name2 = 'snapshot_ftp_user';
     $opt_name3 = 'snapshot_ftp_pass';
     $opt_name4 = 'snapshot_ftp_subdir';
+	$opt_name5 = 'snapshot_ftp_prefix';
+	$opt_name6 = 'snapshot_add_dir1';
     $hidden_field_name = 'snapshot_ftp_hidden';
     $hidden_field_name2 = 'snapshot_backup_hidden';
     $hidden_field_name3 = 'snapshot_check_repo';
@@ -59,16 +61,79 @@ function snapshot() {
     $data_field_name2 = 'snapshot_ftp_user';
     $data_field_name3 = 'snapshot_ftp_pass';
     $data_field_name4 = 'snapshot_ftp_subdir';
+	$data_field_name5 = 'snapshot_ftp_prefix';
+	$data_field_name6 = 'snapshot_add_dir1';
 
     // Read in existing option value from database
     $opt_val = get_option( $opt_name );
     $opt_val2 = get_option ($opt_name2 );
     $opt_val3 = get_option ($opt_name3 );
     $opt_val4 = get_option ($opt_name4 );
+	$opt_val5 = get_option ($opt_name5 );
+	$opt_val6 = get_option ($opt_name6 );
 
 
     // reset working directory to WP root
-    chdir('../');
+    // chdir('../');
+	chdir (ABSPATH);
+	
+	// @since 1.6
+	// let's include some subroutines - doesn't work yet
+// include plugin_dir_path(__FILE__).'includes/test-ftp.php';
+	
+	// create footer
+    function snapshot_footer(){
+    ?>
+    <p><strong>Coming soon: automated backups, repository browser, snapshot restore option. Watch this space! </strong></p>
+    <p>This plugin was brought to you by<br />
+    <a href="http://wpguru.co.uk" target="_blank"><?php echo '<img src="'. plugins_url('images/guru-header.jpg', __FILE__) .'">'; ?></a></p>
+    <p>Snapshot Backup Version 1.6 | <a href="http://wpguru.co.uk/2011/02/snapshot-backup/" target="_blank">Plugin Home Page</a>  | <a href="http://plugins.trac.wordpress.org/log/snapshot-backup/" target="_Blank">Changelog</a> | <a href="http://wpguru.co.uk/hosting/ftp/" target="_blank">Get an FTP Account</a> | <a href="http://wpguru.co.uk/say-thanks/" target="_blank">Buy me a Coffee</a></p>
+    <?php }
+	
+/* 
+ * @since 1.5
+ * ADDITIONAL BACKUP SETTINGS
+ */
+ 
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name3 ]) && $_POST[ $hidden_field_name3 ] == 'Y' ) {
+    // Read their posted value
+    $opt_val6 = trim($_POST[ $data_field_name6 ]);
+	// Save the posted value in the database
+    update_option( $opt_name6, $opt_val6 );
+	// Put a "settings updated" message on the screen
+?>
+<div class="updated"><p><strong><?php echo 'Your additional directory has been saved.'; ?></strong></p></div>
+<?php
+    }
+/*
+ * @since 1.0
+ * FTP FORM SETTINGS
+ */
+	
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+    $opt_val = trim($_POST[ $data_field_name ]);
+    $opt_val2 = trim($_POST[ $data_field_name2 ]);
+	$opt_val3 = trim($_POST[ $data_field_name3 ]);
+    $opt_val4 = trim($_POST[ $data_field_name4 ]);
+	$opt_val5 = trim($_POST[ $data_field_name5 ]);
+        
+	// Save the posted value in the database
+    update_option( $opt_name, $opt_val );
+    update_option( $opt_name2, $opt_val2 );
+	update_option( $opt_name3, $opt_val3 );
+	update_option( $opt_name4, $opt_val4 );
+	update_option( $opt_name5, $opt_val5 );
+
+        // Put a "settings updated" message on the screen
+?>
+<div class="updated"><p><strong><?php _e('Your FTP details have been saved.', 'snapshot-menu' ); ?></strong></p></div>
+<?php
+    }
 
 /****************************************************
 / SNAPSHOT ADMIN AREA
@@ -76,41 +141,44 @@ function snapshot() {
 // Header
 ?>
 <div class="wrap">
-<h2><img src="/wp-content/plugins/snapshot-backup/WP-Guru-Logo.png">&nbsp;Welcome to Snapshot Backup!</h2>
-
+<h2><?php echo '<img src="'. plugins_url('images/WP-Guru-Logo.png', __FILE__) .'">'; ?>&nbsp;Welcome to Snapshot Backup!</h2>
 <table class="snapshot-backup" width=600 cellspacing=10 bgcolor=red>
 <tr><td>
 <p><strong>With this plugin you can create an up-to-the-minute archive of your entire website and save it to an offsite location via FTP.</strong></p>
 <p>Things couldn't be easier: </p>
 <ul><li>&nbsp;&nbsp;&bull; enter your FTP details at the bottom</li>
-<li>&nbsp;&nbsp;&bull; click on CREATE SNAPSHOT</li>
+<li>&nbsp;&nbsp;&bull; click on CREATE NEW SNAPSHOT</li>
 <li>&nbsp;&nbsp;&bull; rest assured you've backed your database AND contents with just one single click</li></ul>
 <p>If you don't have an FTP account you can <a href="http://wpguru.co.uk/hosting/ftp/" target="_blank">sign up for one here</a> or download your snapshot from this server once it's done.</p>
 </td></tr>
 </table>
-<p>Any questions? Check out the included <a href="../wp-content/plugins/snapshot-backup/readme.txt" target="_blank">readme file</a> or visit the <a href="http://wpguru.co.uk/2011/02/snapshot-backup/" target="_blank">Snapshot Backup Website</a>. Have fun!</p>
+<p>Any questions? Check out the included <?php echo '<a href="' . plugins_url('readme.txt', __FILE__) .'"'; ?>" target="_blank">readme file</a> or visit the <a href="http://wpguru.co.uk/2011/02/snapshot-backup/" target="_blank">Snapshot Backup Website</a>. Have fun!</p>
 
 <?php
- if( isset($_POST[ $hidden_field_name2 ]) && $_POST[ $hidden_field_name2 ] == 'Y' ) {
 
-// this could take a while...
+ if( isset($_POST[ $hidden_field_name2 ]) && $_POST[ $hidden_field_name2 ] == 'Y' ) {
+/*
+ * @since 1.0
+ * MAIN SNAPSHOT BUTTON
+ */
 // set the number of seconds you'd like to wait for the script here
-// default is 300
+// default is 300 
 set_time_limit(300);
 
 // create global file name
 $filetime = date('Ymd-Gi');
-// echo "<br>The Filetime is $filetime <br>";
+// call pre-flight checklist
+include plugin_dir_path(__FILE__).'includes/preflight.php';
 // readout the Database
-include 'wp-content/plugins/snapshot-backup/database.php';
+include plugin_dir_path(__FILE__).'includes/database.php';
 // create ZIP package
-include 'wp-content/plugins/snapshot-backup/zipshot.php';
+include plugin_dir_path(__FILE__).'includes/zipshot.php';
 // send package to FTP
-include 'wp-content/plugins/snapshot-backup/sendaway.php';
+include plugin_dir_path(__FILE__).'includes/sendaway.php';
 // we're done
-echo '<div class="updated"><h2>All done - thank you!</h2>';
+echo '<div class="updated"><h2>All done - thank you for using Snapshot Backup!</h2>';
 ?>
-<p><strong>You can download your snapshot file here: <a href="../wp-content/uploads/<?php echo get_option('snapshot_latest'); ?>"><?php echo get_option('snapshot_latest'); ?></a></strong></p></div>
+</div>
 <?php
 }
 
@@ -119,24 +187,25 @@ echo '<div class="updated"><h2>All done - thank you!</h2>';
 <form name="form2" method="post" action="">
 <input type="hidden" name="<?php echo $hidden_field_name2; ?>" value="Y">
 <p class="submit">
-<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Create Snapshot Backup') ?>" />
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Create New Snapshot') ?>" />
 </p>
 </form>
 <hr />
 
 <?php
-// check FTP Repository for existing backups
-// include 'wp-content/plugins/snapshot-backup/list-repo.php'; 
-// call FTP Details form
-include 'wp-content/plugins/snapshot-backup/ftp-form.php';
-
-// footer
-?>
-<hr />
-<p>This plugin was brought to you by<br />
-<a href="http://wpguru.co.uk" target="_blank"><img src="/wp-content/plugins/snapshot-backup/guru-header.jpg"></a>
-</p>
-<p><a href="http://wpguru.co.uk/2011/02/snapshot-backup/" target="_blank">Plugin Home Page</a> | <a href="http://davidwalsh.name/backup-mysql-database-php" target="_blank">Database Script by the amazing David Walsh</a> | <a href="http://wpguru.co.uk/hosting/ftp/" target="_blank">Get an FTP Account</a> | <a href="http://wpguru.co.uk/say-thanks/" target="_blank">Buy me a Coffee</a></p>
-
-<?php
+// call Recent Download option
+if (get_option('snapshot_latest')){
+include plugin_dir_path(__FILE__).'includes/download-recent.php';
 }
+// call FTP Details form
+include plugin_dir_path(__FILE__).'includes/ftp-form.php';
+
+// call Backup Settings
+include plugin_dir_path(__FILE__).'includes/settings.php';
+
+// call footer
+snapshot_footer();
+
+// end of Plugin
+}
+?>
